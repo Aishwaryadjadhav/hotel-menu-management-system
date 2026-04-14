54c49107c7bc98b19ccd4a8c7b97541457435810
Aishwaryadjadhav <aishwaryadj1@gmail.com>
2026-04-14 17:56:26 +0530

    Added POST API and MenuItem model

--- src/main/java/com/hotel/menu_management/controller/MenuController.java
+++ src/main/java/com/hotel/menu_management/controller/MenuController.java
@@ -1,9 +1,13 @@
 package com.hotel.menu_management.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import com.hotel.menu_management.model.MenuItem;
 
 @RestController
 public class MenuController {
@@ -19,5 +23,14 @@ public class MenuController {
 		return List.of("pizza", "burger", "pasta");
 		
 	}
+	
+	List<MenuItem> menulist = new ArrayList<>();
 
+	@PostMapping ("/menu/add")
+	public String addMenuItem(@RequestBody MenuItem item) {
+		menulist.add(item);
+		return "item added succesfully";
+		
+	}
+	
 }
